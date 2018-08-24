46c5f10546a19b2049e051ac74cfc470f5cc5a31
Hrushi <hrushi@git.com>
2018-08-24 21:47:29 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+
+hbs.registerHelper('c_year', () => {
+  return new Date().getFullYear()
+});
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  let log = `${now} : ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err)
+    {
+      console.log('unable to write file server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintainance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+  // res.send('Hey There!!!');
+  // res.send({
+  //   name: 'hrushikesh',
+  //   likes: ['playing games', 'coding', 'movies']
+  // });
+  res.render('home.hbs',{
+    title: 'home'
+  })
+});
+
+app.get('/about', (req,res) => {
+  // res.send('About page');
+  res.render('about.hbs',{
+    title: 'ABOUT PAGE'
+  });
+})
+app.listen(8000, () => {
+  console.log('server is up on localhost:8000');
+});
